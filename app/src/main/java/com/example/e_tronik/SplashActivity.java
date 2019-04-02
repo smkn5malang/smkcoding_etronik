aaba139a475f89bde09ceb576cf1ec0f738e80d5
adlubagusi <adlubagusi@gmail.com>
2019-04-02 17:52:37 +0700

    Adding Splash screen

---
+++ app/src/main/java/com/example/e_tronik/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.example.e_tronik;
+
+import android.content.Intent;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        SystemClock.sleep(3000);
+        Intent mainIntent = new Intent(this, MainActivity.class);
+        startActivity(mainIntent);
+        finish();
+    }
+}
